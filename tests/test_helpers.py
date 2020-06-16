7709e88dda09c7874ff6cf7aa873752a2c4a7e8e
fkoh111 <frederik_kok@icloud.com>
2020-06-17 00:04:58 +0200

    Added test mail config

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,3 +1,13 @@
+from commons.helpers import MailConfig
+
+
 def test_smoke():
     """Smoke test"""
     assert 2 + 2 == 4
+
+
+def test_mail_config():
+    """Testing mail config attributes"""
+    mailconfig = MailConfig
+    assert (mailconfig.SENDER) == "fkhonlinemail@gmail.com"
+    assert (mailconfig.RECEIVER) == "frederik_kok@icloud.com"
